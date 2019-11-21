0e14a24e5cd279bcc00894a928a64f7fb34802a2
Lau <LauDaniel6872@students.esuhsd.org>
2019-11-21 10:08:35 -0800

    update server.js

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const ws = require('ws');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const httpServer = http.createServer(app);
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+httpServer.listen(process.env.PORT || 1910);
