32a13a2b3c88332e30401e473ef7669bf8f6eb01
Lau <LauDaniel6872@students.esuhsd.org>
2019-11-25 09:36:43 -0800

    Updating more files, adding firebase stuff... - looks like cannot test on school computers

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ws = require('ws');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const firebase = require('firebase');
 const app = express();
 const hash = require(path.join(__dirname,"lib","hash.js"));
 const httpServer = http.createServer(app);
@@ -13,3 +14,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 httpServer.listen(process.env.PORT || 1910);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyAxEc43oDpJms036wVOeKdjvNsQLszvU",
+  authDomain: "bepreparedness.firebaseapp.com",
+  databaseURL: "https://bepreparedness.firebaseio.com",
+  projectId: "bepreparedness",
+  storageBucket: "bepreparedness.appspot.com",
+  messagingSenderId: "106107649883",
+  appId: "1:106107649883:web:fece6ab1530c3ee3e8647e",
+  measurementId: "G-16NGQBRXK7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
