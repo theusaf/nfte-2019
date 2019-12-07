f429043de9288f054e926eb6f0cb11d4ce8d005a
theusaf <theusafyt@gmail.com>
2019-12-07 12:18:34 -0800

    Updated sandstorm help page.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ws = require('ws');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-//const firebase = require('firebase');
+const ip = require('ip');
 const app = express();
 const hash = require(path.join(__dirname,"lib","hash.js"));
 const httpServer = http.createServer(app);
@@ -21,15 +21,4 @@ app.get("*",(req,res)=>{
 
 httpServer.listen(process.env.PORT || 1910);
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDyAxEc43oDpJms036wVOeKdjvNsQLszvU",
-  authDomain: "bepreparedness.firebaseapp.com",
-  databaseURL: "https://bepreparedness.firebaseio.com",
-  projectId: "bepreparedness",
-  storageBucket: "bepreparedness.appspot.com",
-  messagingSenderId: "106107649883",
-  appId: "1:106107649883:web:fece6ab1530c3ee3e8647e",
-  measurementId: "G-16NGQBRXK7"
-};
-//Initialize Firebase
-firebase.initializeApp(firebaseConfig);*/
+console.log(ip.address());
