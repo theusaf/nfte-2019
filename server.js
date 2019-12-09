00bb275c920161a773dd4a442c8a2714ef21007b
theusaf <theusafyt@gmail.com>
2019-12-09 09:53:37 -0800

    removing unused packages (ws / request) from package.json

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const request = require('request');
 const fs = require('fs');
 const path = require('path');
-const ws = require('ws');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
