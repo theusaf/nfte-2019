d0808ee93af811bac079de0e3b1971f08e1f696b
theusaf <theusafyt@gmail.com>
2019-12-09 09:46:49 -0800

    Add weather api

--- server.js
+++ server.js
@@ -8,12 +8,37 @@ const bodyParser = require('body-parser');
 const ip = require('ip');
 const app = express();
 const hash = require(path.join(__dirname,"lib","hash.js"));
+const weather = require('weather-js');
 const httpServer = http.createServer(app);
 app.use(express.static("public",{
   maxAge: "1y"
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/weather",(req,res)=>{
+  const body = req.query;
+  if(body.loc){
+    weather.find({
+      search: body.loc,
+      degreeType: body.deg || "F"
+    },(err,resp)=>{
+      if(err){
+        return res.send(JSON.stringify({
+          code: 500,
+          error: "Weather service error"
+        }));
+      }
+      res.send(JSON.stringify(resp,null,1));
+    });
+  }else{
+    res.send(JSON.stringify({
+      code: 400,
+      error: "No Location Provided"
+    }));
+  }
+});
+
 // 404 Page
 app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname,"public","404.html"));
