5b660c074212068d8c74ed3bcbd675a28b7242bf
theusaf <theusafyt@gmail.com>
2019-12-12 09:06:35 -0800

    Adding about page, added forecast

--- public/information/index.js
+++ public/information/index.js
@@ -26,19 +26,33 @@ function getWeather(loc){
 }
 async function renderWeatherForecast(){
   getUserLocation().then(async o=>{
+    // Today's weather
     const response = await getWeather(o);
     const data = JSON.parse(response)[0];
     const now = data.current;
     const forecast = data.forecast;
-    document.getElementById("image").querySelector("img").src = now.imageUrl;
+    const weImage = document.getElementById("image").querySelector("img");
+    weImage.src = now.imageUrl;
+    weImage.style.display = "inline-block";
     const template = document.createElement("template");
     template.innerHTML = `
-    <h3>Weather for ${now.day}, ${now.date}</h3>
-    <h4>${now.skytext} skies</h4>
-    <h4>Temperature: ${now.temperature} F</h4>
-    <h4>Wind: ${now.winddisplay}</h4>
+    <h2 style="display: inline-block;">Weather for ${now.day}, ${now.date}</h2>
+    <div class="indent">
+      <h3>${now.skytext} skies</h3>
+      <h3>Temperature: ${now.temperature} F</h3>
+      <h3>Wind: ${now.winddisplay}</h3>
+      <h3>Humidity: ${now.humidity}</h3>
+    </div>
     `;
     document.getElementById("image").append(template.content.cloneNode(true));
+    // Forecast
+    const forcast = document.createElement("template");
+    forcast.innerHTML = `
+    <div class="indent row-flex flex" style="flex: 1">
+      ${ForecastHTML(forecast)}
+    </div>
+    `;
+    document.getElementById("forecast").append(forcast.content.cloneNode(true));
   }).catch(e=>{
     if(e == false){
       // no geolocation provided
@@ -48,3 +62,20 @@ async function renderWeatherForecast(){
   });
 }
 renderWeatherForecast();
+
+function ForecastHTML(data){
+  let str = "";
+  for(let i = 0; i<data.length;++i){
+    str+=`
+    <div class="forecast">
+      <img src="http://blob.weather.microsoft.com/static/weather4/en-us/law/${data[i].skycodeday}.gif">
+      <h4>${data[i].shortday}, ${data[i].date}</h4>
+      <h5>${data[i].skytextday} skies</h5>
+      <h5>High: ${data[i].high} F</h5>
+      <h5>Low: ${data[i].low} F</h5>
+      <h5>Precipitation Chance: ${data[i].precip || data[i].precip === 0 ? data[i].precip + "%" : "unknown"}</h5>
+    </div>
+    `
+  }
+  return str;
+}
